b6a748a508e53de01e0fd44170755a2dcfde111a
SashaPlavi <sasha.bingulac@gmail.com>
2019-12-25 22:10:48 +0100

    adding jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>helloo</h1>
+    <HelloWorld msg="cao bre" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/?_limit=10")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
